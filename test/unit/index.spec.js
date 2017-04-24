2d67b6eff0c07f3c22c6e6c2babb0cfa33bd1a3b
Scott Prue <scott@residenetwork.com>
2017-04-23 18:22:21 -0700

    Version v0.2.0
    * Includes #1 (support label prop)
    * Added yarn file
    * Yarn cache settings added to travis file

--- test/unit/index.spec.js
+++
@@ -1,8 +0,0 @@
-/* global describe, it, expect */
-describe('react-google-button Library', () => {
-  describe('constructor', () => {
-    it('has unit test scaffolding', () => {
-      expect(1).to.equal(1)
-    })
-  })
-})
