7fdd98407c1f3ec262605a988e03a92283d5d519
prescottprue <sprue.dev@gmail.com>
2017-05-24 01:29:39 -0700

    Disabled styling. Unit tests now include testing rendered output.

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,25 +1,92 @@
 import React from 'react'
 import GoogleButton from '../../src'
-// import { shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 
-/* global describe, it, expect, beforeEach */
+const defaults = {
+  label: 'Sign in with Google',
+  backgroundColor: '#4285f4'
+}
+
+const disabledStyle = {
+  backgroundColor: 'rgba(0, 0, 0, .08)'
+}
+
 describe('react-google-button Library', () => {
   describe('exports a component', () => {
     expect(GoogleButton).to.exist
   })
-  // describe('(Component) AddressForm', () => {
-  //   let _component
-  //
-  //   beforeEach(() => {
-  //     _component = shallow(
-  //       <GoogleButton
-  //       />
-  //     )
-  //   })
-  //
-  //   it('Renders div', () => {
-  //     const firstDiv = _component.find('div')
-  //     expect(firstDiv).to.exist
-  //   })
-  // })
+  describe('(Component) AddressForm', () => {
+    let _component
+
+    describe('Default Props', () => {
+      beforeEach(() => {
+        _component = shallow(
+          <GoogleButton
+          />
+        )
+      })
+      it('Renders div', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv).to.exist
+      })
+
+      it('Sets correct label text', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv.first().text()).to.equal(defaults.label)
+      })
+
+      it('Applies styles', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv.first())
+          .to
+          .have
+          .style('background-color', defaults.backgroundColor)
+      })
+    })
+
+    describe('Custom Label', () => {
+      const customLabel = 'Custom Label'
+
+      beforeEach(() => {
+        _component = shallow(
+          <GoogleButton
+            label={customLabel}
+          />
+        )
+      })
+
+      it('Renders div', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv).to.exist
+      })
+
+      it('Sets correct label text', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv.first().text()).to.equal(customLabel)
+      })
+    })
+
+    describe('Disabled', () => {
+      beforeEach(() => {
+        _component = shallow(
+          <GoogleButton
+            disabled
+          />
+        )
+      })
+
+      it('Renders div', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv).to.exist
+      })
+
+      it('Applies background color', () => {
+        const firstDiv = _component.find('div')
+        expect(firstDiv.first())
+          .to
+          .have
+          .style('background-color', disabledStyle.backgroundColor)
+      })
+    })
+  })
 })
