a8a5984fad3a342ef44a4882830968ea7b389e03
Scott Prue <scott@residenetwork.com>
2017-05-26 12:45:43 -0700

    Disabled state now has correct icon image. Light style now supported.

--- src/GoogleButton.js
+++ src/GoogleButton.js
@@ -1,84 +1,23 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
-const style = {
-  backgroundColor: '#4285f4',
-  color: '#fff',
-  height: '50px',
-  width: '240px',
-  border: 'none',
-  textAlign: 'center',
-  verticalAlign: 'center',
-  boxShadow: '0 2px 4px 0 rgba(0,0,0,.25)',
-  fontSize: '16px',
-  lineHeight: '48px',
-  display: 'block',
-  borderRadius: '1px',
-  transition: 'background-color .218s, border-color .218s, box-shadow .218s',
-  fontFamily: 'Roboto,arial,sans-serif',
-  cursor: 'pointer'
-}
-
-const iconStyle = {
-  width: '48px',
-  height: '48px',
-  textAlign: 'center',
-  verticalAlign: 'center',
-  display: 'block',
-  marginTop: '1px',
-  marginLeft: '1px',
-  float: 'left',
-  backgroundColor: '#fff',
-  borderRadius: '1px',
-  whiteSpace: 'nowrap'
-}
-
-const svgWrapperStyle = { padding: '15px' }
-
-const svgStyle = {
-  width: '18px',
-  height: '18px',
-  display: 'block'
-}
-
-const hoverStyle = {
-  boxShadow: '0 0 3px 3px rgba(66,133,244,.3)',
-  backgroundColor: '#4285F4',
-  transition: 'background-color .218s, border-color .218s, box-shadow .218s'
-}
-
-// const pressedStyle = {
-//   backgroundColor: '#3367D6'
-// }
-
-const disabledStyle = {
-  backgroundColor: 'rgba(0, 0, 0, .08)',
-  color: 'rgba(0, 0, 0, .40)',
-  cursor: 'not-allowed'
-}
-
-const defaultSvg = (
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 48 48" style={svgStyle}>
-    <g>
-      <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
-      <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
-      <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
-      <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
-      <path fill="none" d="M0 0h48v48H0z"></path>
-    </g>
-  </svg>
-)
+import { GoogleIcon } from './icons'
+import { darkStyle, lightStyle, disabledStyle, hoverStyle } from './styles'
 
 export default class GoogleButton extends Component {
   static propTypes = {
     label: PropTypes.string,
     disabled: PropTypes.bool,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    type: PropTypes.oneOf([
+      'light',
+      'dark'
+    ])
   }
 
   static defaultProps = {
     label: 'Sign in with Google',
     disabled: false,
+    type: 'dark',
     onClick: () => {}
   }
 
@@ -87,13 +26,14 @@ export default class GoogleButton extends Component {
   }
 
   getStyle = () => {
+    const baseStyle = this.props.type === 'dark' ? darkStyle : lightStyle
     if (this.state.hovered) {
-      return { ...style, ...hoverStyle }
+      return { ...baseStyle, ...hoverStyle }
     }
     if (this.props.disabled) {
-      return { ...style, ...disabledStyle }
+      return { ...baseStyle, ...disabledStyle }
     }
-    return style
+    return baseStyle
   }
 
   mouseOver = () => {
@@ -125,13 +65,7 @@ export default class GoogleButton extends Component {
         onMouseOver={this.mouseOver}
         onMouseOut={this.mouseOut}
       >
-        <div style={iconStyle}>
-          <div style={svgWrapperStyle}>
-            {defaultSvg}
-          </div>
-          {/* <UseIcon /> */}
-          {/* <UseIcon2 /> */}
-        </div>
+        <GoogleIcon {...this.props} />
         <span>{label}</span>
       </div>
     )
