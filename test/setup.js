29d7802bea2cf2c1eebfa80a5a3adb35b33c597a
Scott Prue <scott@residebrokerage.com>
2019-12-09 02:05:03 -0500

    feat(test): add tests for onClick prop and hover styles

--- test/setup.js
+++ test/setup.js
@@ -1,22 +1,14 @@
 /* eslint-disable no-unused-vars */
 const chai = (global.chai = require('chai'))
+const sinon = (global.sinon = require('sinon'))
 const expect = (global.expect = chai.expect)
 const should = (global.should = chai.should())
 const enzyme = require('enzyme')
 const chaiEnzyme = require('chai-enzyme')
 const Adapter = require('enzyme-adapter-react-16')
+const sinonChai = require('sinon-chai')
 
 enzyme.configure({ adapter: new Adapter() })
-// var Promise = require('es6-promise').Promise
-// global.Promise = Promise
 
-// var chaiAsPromised = require('chai-as-promised')
-// chai.use(chaiAsPromised)
 chai.use(chaiEnzyme())
-
-// var jsdom = require('jsdom')
-// var doc = jsdom.jsdom('<html><body></body></html>')
-// var win = doc.defaultView
-// global.document = doc
-// global.window = win
-// global.navigator = win.navigator
+chai.use(sinonChai)
