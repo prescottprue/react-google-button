861c9dda59657c7f2adfae6f6ae12a7914ed72b6
Scott Prue <prescottprue@users.noreply.github.com>
2024-05-03 16:02:40 -0600

    feat: function component with defaultProps (#57) (#58)

--- src/GoogleButton.js
+++ src/GoogleButton.js
@@ -1,74 +1,67 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { GoogleIcon } from './icons'
 import { darkStyle, lightStyle, disabledStyle, hoverStyle } from './styles'
 
-export default class GoogleButton extends PureComponent {
-  static propTypes = {
-    label: PropTypes.string,
-    disabled: PropTypes.bool,
-    tabIndex: PropTypes.number,
-    onClick: PropTypes.func,
-    type: PropTypes.oneOf(['light', 'dark']),
-    style: PropTypes.object
-  }
-
-  static defaultProps = {
-    label: 'Sign in with Google',
-    disabled: false,
-    type: 'dark',
-    tabIndex: 0,
-    onClick: () => {}
-  }
-
-  state = {
-    hovered: false
-  }
-
-  getStyle = propStyles => {
-    const baseStyle = this.props.type === 'dark' ? darkStyle : lightStyle
-    if (this.state.hovered) {
-      return { ...baseStyle, ...hoverStyle, ...propStyles }
-    }
-    if (this.props.disabled) {
-      return { ...baseStyle, ...disabledStyle, ...propStyles }
-    }
-    return { ...baseStyle, ...propStyles }
-  }
-
-  mouseOver = () => {
-    if (!this.props.disabled) {
-      this.setState({ hovered: true })
+export default function GoogleButton({
+  label = 'Sign in with Google',
+  disabled = false,
+  tabIndex = 0,
+  onClick = () => {},
+  type = 'dark',
+  style
+}) {
+  const [hovered, setHovered] = useState(false)
+  const mouseOver = () => {
+    if (!disabled) {
+      setHovered(true)
     }
   }
 
-  mouseOut = () => {
-    if (!this.props.disabled) {
-      this.setState({ hovered: false })
+  const mouseOut = () => {
+    if (!disabled) {
+      setHovered(false)
     }
   }
 
-  click = e => {
-    if (!this.props.disabled) {
-      this.props.onClick(e)
+  const getStyle = () => {
+    const baseStyle = type === 'dark' ? darkStyle : lightStyle
+    if (hovered) {
+      return { ...baseStyle, ...hoverStyle, ...style }
+    }
+    if (disabled) {
+      return { ...baseStyle, ...disabledStyle, ...style }
+    }
+    return { ...baseStyle, ...style }
+  }
+
+  const handleClick = (e) => {
+    if (!disabled) {
+      onClick(e)
     }
   }
 
-  render() {
-    const { label, style, ...otherProps } = this.props
+  return (
+    <div
+      disabled={disabled}
+      tabIndex={tabIndex}
+      onClick={handleClick}
+      role="button"
+      style={getStyle()}
+      onMouseOver={mouseOver}
+      onMouseOut={mouseOut}
+    >
+      <GoogleIcon disabled={disabled} type={type} />
+      <span>{label}</span>
+    </div>
+  )
+}
 
-    return (
-      <div
-        {...otherProps}
-        role="button"
-        onClick={this.click}
-        style={this.getStyle(style)}
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-      >
-        <GoogleIcon {...this.props} />
-        <span>{label}</span>
-      </div>
-    )
-  }
+GoogleButton.propTypes = {
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+  tabIndex: PropTypes.number,
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(['light', 'dark']),
+  style: PropTypes.object
 }
